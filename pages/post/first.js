5a209a65d659f094f76e774e58aa956d1cffa8fc
osareniye osazee <osareniyeosazee@gmail.com>
2023-10-03 17:39:09 +0100

    building a blog using next

---
+++ pages/post/first.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+export default function FirstPost() {
+    return( 
+    <div>
+        <h2>hello wprld</h2>
+       
+        <Link href="/">this page!</Link>
+    </div>
+    )
+  }
